981d2dd3ec6f4565413ae5a0005328333268b6b6
Redex68 <marin.petric73@gmail.com>
2023-11-18 02:34:50 +0100

    Finished broadcast implementation

--- Assets/Scripts/Clients.cs
+++ Assets/Scripts/Clients.cs
@@ -1,13 +1,14 @@
 using System.Net.Sockets;
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System.Net;
+using System;
 
 public static class Clients
 {
     public static List<IPAddress> adapterAddresses = new();
     public static List<IPAddress> serverAddresses = new();
-    public static Dictionary<IPAddress, UdpClient> adapters = new();
+    public static Dictionary<IPAddress, Tuple<UdpClient, IPEndPoint>> adapters = new();
     public static Dictionary<IPAddress, UdpClient> servers = new();
+    public static Tuple<UdpClient, IPEndPoint> currentAdapter = null;
+    public static UdpClient currentServer = null;
 }
--- Assets/Scripts/Discovery.cs
+++ Assets/Scripts/Discovery.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -7,79 +7,113 @@ using UnityEngine.Events;
 
 public class Discovery : MonoBehaviour
 {
-    [SerializeField] int Port = 42069;
+    [SerializeField] int BroadcastPort = 42069;
     [SerializeField] string Request = "Ola";
     [SerializeField] TMPro.TMP_Dropdown adaptersDropdown;
 
     public UnityEvent<UdpReceiveResult> serverFound;
-    public static List<UdpClient> clients = new();
-    //Currently selected client
-    UdpClient client = null;
-    IPEndPoint BroadcastEp;
     byte[] RequestData;
     
     // Start is called before the first frame update
     void Start()
     {
-        BroadcastEp = new IPEndPoint(IPAddress.Broadcast, Port);
         RequestData = Encoding.ASCII.GetBytes(Request);
         ReadAdapters();
         adaptersDropdown.onValueChanged.AddListener(SetAdapter);
+
+        BroadcastResponseListen();
     }
 
     private void ReadAdapters()
     {
-        List<string> addresses = new();
         IPHostEntry hostEntry=Dns.GetHostEntry(Dns.GetHostName());
         foreach (IPAddress ip in hostEntry.AddressList) {
             if (ip.AddressFamily==AddressFamily.InterNetwork) {
-                addresses.Add(ip.ToString());
-                Debug.Log(ip);
-                UdpClient tmp = new UdpClient(new IPEndPoint(IPAddress.Parse(ip.ToString()), Port))
+                UdpClient tmp = new UdpClient(new IPEndPoint(ip, BroadcastPort))
                 {
-                    EnableBroadcast = true
+                    EnableBroadcast = true,
                 };
                 tmp.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
                 tmp.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.DontRoute, 1);
-                clients.Add(tmp);
+                tmp.Client.ReceiveTimeout = 1000;
+
+                Clients.adapterAddresses.Add(ip);
+                byte[] ipBytes = ip.GetAddressBytes();
+                ipBytes[3] = 255;
+                IPEndPoint ep = new IPEndPoint(new IPAddress(ipBytes), BroadcastPort);
+                Clients.adapters.Add(ip, new Tuple<UdpClient, IPEndPoint>(tmp, ep));
             }
         }
 
         adaptersDropdown.ClearOptions();
-        adaptersDropdown.AddOptions(addresses);
-        client = clients[0];
+        adaptersDropdown.AddOptions(Clients.adapterAddresses.ConvertAll(ip => ip.ToString()));
+        Clients.currentAdapter = Clients.adapters[Clients.adapterAddresses[0]];
     }
 
-/// <summary>
-/// Called when the user selects a new network adapter
-/// </summary>
-/// <param name="ipAddr">The IP address of the adapter</param>
+/// <summary> Called when the user selects a new network adapter </summary>
+/// <param name="indx"> The index of the network adapter's IP address in the list of adapter IP's </param>
     public void SetAdapter(int indx)
     {
-        client = clients[indx];
+        IPAddress addr = Clients.adapterAddresses[indx];
+        Clients.currentAdapter = Clients.adapters[addr];
     }
 
-    public void FindServer()
+    public void BroadcastRequest()
     {
-        FindServerAsync();
+        BroadcastRequestAsync();
     }
 
-    private async void FindServerAsync()
+    private async void BroadcastRequestAsync()
     {
-        if(client != null)
+        if(Clients.currentAdapter != null)
         {
-            Debug.Log($"Sending broadcast to port {Port}");
-            await client.SendAsync(RequestData, RequestData.Length, BroadcastEp);
-
-            UdpReceiveResult result = await client.ReceiveAsync();
-
-            Debug.Log($"Server: {result.RemoteEndPoint}");
-            Debug.Log($"Data: {Encoding.ASCII.GetString(result.Buffer)}");
-            serverFound.Invoke(result);
+            Debug.Log($"Sending broadcast to {Clients.currentAdapter.Item2}");
+            await Clients.currentAdapter.Item1.SendAsync(RequestData, RequestData.Length, Clients.currentAdapter.Item2);
         }
         else
         {
             Debug.LogError("Client is not defined, pick an adapter");
+        }
+    }
+
+/// <summary> Listens on the currently selected adapter for a response to a broadcast request. </summary>
+    private async void BroadcastResponseListen()
+    {
+        while(true)
+        {
+            try
+            {
+                Debug.Log("Awaiting");
+                UdpReceiveResult result = await Clients.currentAdapter.Item1.ReceiveAsync();
+                string response = Encoding.ASCII.GetString(result.Buffer);
+
+                Debug.Log($"Server: {result.RemoteEndPoint}");
+                Debug.Log($"Data (The connection port): {response}");
+
+                //Ignore own broadcast
+                if(response == Request) continue;
+
+                IPAddress ip = result.RemoteEndPoint.Address;
+                int port = int.Parse(Encoding.ASCII.GetString(result.Buffer));
+
+                if(!Clients.serverAddresses.Contains(ip))
+                {
+                    UdpClient serverClient = new UdpClient();
+                    serverClient.Connect(ip, port);
+
+                    Clients.serverAddresses.Add(ip);
+                    Clients.servers.Add(ip, serverClient);
+                    if(Clients.currentServer == null)
+                        Clients.currentServer = serverClient;
+                    
+                    result.RemoteEndPoint.Port = port;
+                    serverFound.Invoke(result);
+                }
+
+            } catch(Exception e)
+            {
+                Debug.LogError(e);
+            }
         }
     }
 }
