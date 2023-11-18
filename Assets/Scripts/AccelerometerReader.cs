3829fbac9063dad55670df6f4eb6397280af8775
Redex68 <marin.petric73@gmail.com>
2023-11-18 02:35:07 +0100

    Implemented accelerometer server

--- Assets/Scripts/AccelerometerReader.cs
+++ Assets/Scripts/AccelerometerReader.cs
@@ -1,11 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+using System.Net;
+using System.Text;
 using UnityEngine;
 
 public class AccelerometerReader : MonoBehaviour
 {
     [SerializeField] TMPro.TMP_Dropdown serverDropdown;
+    
+    public bool connected {get; private set;} = false;
     
     void Start()
     {
@@ -15,11 +16,26 @@ public class AccelerometerReader : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(connected)
+        {
+            byte[] data = Encoding.ASCII.GetBytes($"{Input.acceleration.x} {Input.acceleration.y} {Input.acceleration.z}");
+            Clients.currentServer.SendAsync(data, data.Length);
+        }
     }
 
     public void ServerSelected(int indx)
     {
-        
+        IPAddress serverIP = Clients.serverAddresses[indx];
+        Clients.currentServer = Clients.servers[serverIP];
+    }
+
+    public void StartSend()
+    {
+        connected = true;
+    }
+
+    public void StopSend()
+    {
+        connected = false;
     }
 }
